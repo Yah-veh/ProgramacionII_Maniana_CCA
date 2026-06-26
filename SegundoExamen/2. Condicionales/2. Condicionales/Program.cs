34158f9f896e50bc2fd7399388903271c7da1505
Yah-veh <yersonmlisarazu@gmail.com>
2026-06-26 11:37:48 -0400

    Actualiznado repo

---
+++ SegundoExamen/2. Condicionales/2. Condicionales/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el dia");
+            int aux = int.Parse(Console.ReadLine());
+            switch ( aux)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Lunes");
+                        break;
+                    }
+                    default:
+                    {
+                        Console.WriteLine("Ingrese un numero");
+                        break;
+                    }
+
+            }
+
+            /*
+
+            Console.WriteLine("Ingrese la nota");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num >= 90)
+            {
+                Console.WriteLine("Excelente");
+            }
+            else if (num >= 70)
+            {
+                Console.WriteLine("Bueno");
+            }
+            else if (num >= 51)
+            {
+                Console.WriteLine("Aprobado");
+            }
+            else
+            {
+                Console.WriteLine("Reprobado");
+            }
+
+
+            /*
+            Console.WriteLine("Ingrese el numero");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0) {
+                Console.WriteLine("Par");
+            }
+            else {
+                    Console.WriteLine("Impar");
+            }
+            */
+        }
+    }
+}
---
+++ SegundoExamen/3. Ciclos/3. Ciclos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Ciclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su clave");
+            int clave = int.Parse(Console.ReadLine());
+
+            while(clave != 1234)
+            {
+                Console.WriteLine("Clave Incorrecta. Intente de nuevo");
+                clave = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Bienvenido al Sistema");
+            
+            
+            /*
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Numero "+ i);
+            }
+            */
+
+        }
+    }
+}
